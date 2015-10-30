9335bcff22ab4006262984510ac509d87f03ebf6
RadiantBlur <pdavis3222@gmail.com>
2015-10-30 18:17:55 -0500

    Updated

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var wins = 0;
 var losses = 0;
 var calibrated = false;
 var calibratingMatches = 0;
+var games = [ ];
 
 function write() {
 
@@ -30,12 +31,16 @@ function write() {
 		document.getElementById("notyet").id = " ";	
 	}
 
-	
-
 	if (morale < 0) {
 		alert("Your morale was too low so you died.")
 		document.body.innerHTML = ""
 	}
+
+	console.log(calibratingMatches);
+}
+
+function db() {
+	document.getElementById("db").innerHTML += "<tr> <td>"+ games[matches][3] + "</td> <td>" + games[matches][0] + "</td><td>" + games[matches][1] + "</td> <td>" + games[matches][2] + "</td> <td>" + games[matches][4] + "</td></tr>";	
 }
 
 var random = function(min,max) {
@@ -49,99 +54,92 @@ var averageKDA = [ ];
 var kdaCount = 0;
 
 function normalGame() {
-	/*if (random(0,1) === 0) {
-		if (!calibrated) {
-			mmr += 25;
-		}
-		var kda = [random(1,20),random(1,20),random(1,20)];
-		averageKDA[kdaCount] = (kda[0] + kda[2]) / kda[1]
-		alert("You won with " + lanes[random(0,4)] + " " + heroes[random(0,109)] + " with a score of " + kda[0] + "/" + kda[1] + "/" + kda[2]);
-		morale += 5;
-		won = true;
-		kdaCount++;
-		wins++;
-	}
-	else {
-		if (!calibrated) {
-			mmr -= 25;
-		}
-		var kda = [random(1,20),random(1,20),random(1,20)];
-		averageKDA[kdaCount] = (kda[0] + kda[2]) / kda[1]
-		alert("You lost with " + lanes[random(0,4)] + " " + heroes[random(0,109)] + " with a score of " + kda[0] + "/" + kda[1] + "/" + kda[2]);		
-		morale -= 5;	
-		won = false;
-		kdaCount++;
-		losses++;
-	}
-	matches++;
-	console.log(mmr);
-	write();
-	*/
 	game(false,false);
 }
 
-function game(calibrated, ranked) {
-		var kda = [random(1,20),random(1,20),random(1,20)];
-		var hero = heroes[random(0,109)];
-		averageKDA[kdaCount] = (kda[0] + kda[2]) / kda[1];
+function game(c, ranked) {
+	var kda = [random(1,20),random(1,20),random(1,20)];
+	var hero = heroes[random(0,109)];
+	averageKDA[kdaCount] = (kda[0] + kda[2]) / kda[1];
+	var result;
+
 	if (random(0,1) === 0) {
+		result = true;
+	}
+	else {
+		result = false;
+	}
 
-		if (!calibrated && !ranked) {
+	if (result) {
+
+		if (!c && !ranked) {
 			mmr += 25;
 		}
 
-		if (calibrated && ranked) {
+		if (c && ranked) {
 			mmr += 25;
 		}
 
-		if (!calibrated && ranked) {
+		if (!c && ranked) {
 			if ( ((kda[0] + kda[2]) / kda[1]) > 1) {
 				mmr += (kda[0] + kda[2]) / kda[1] * 50;
 			}
 			else {
 				mmr += 150;
 			} 
+			calibratingMatches++;
 		}
 		alert("You won with " + lanes[random(0,4)] + " " + hero + " with a score of " + kda[0] + "/" + kda[1] + "/" + kda[2]);
 		won = true;
-		morale += 5;
+		if (morale < 96) {
+			morale += 5;
+		}
 		wins++;
+		result = "Won";
 	}
 	else {
-		if (!calibrated && !ranked) {
+		if (!c && !ranked) {
 			mmr -= 25;
 		}
 
-		if (calibrated && ranked) {
+		if (c && ranked) {
 			mmr -= 25;
 		}
 
-		if (!calibrated && ranked) {
+		if (!c && ranked) {
 			if ( ((kda[0] + kda[2]) / kda[1]) < 1) {
 				mmr -= 150;
 			}
 			else {
 				mmr += (kda[0] + kda[2]) / kda[1] * 12.5;
 			}
-
-			
+			calibratingMatches++;	
 		}
 		alert("You lost with " + lanes[random(0,4)] + " " + hero + " with a score of " + kda[0] + "/" + kda[1] + "/" + kda[2]);
 		losses++;
 		won = false;
-		morale -= 5;
+
+		result = "Lost";
+
+		if (morale > 5) {
+			morale -= 5;
+		}
 	}
-	calibratingMatches++;
-	if (calibratingMatches > 10) {
-		alert("Your mmr is: " + mmr);
+
+	if (calibratingMatches > 9 && !calibrated) {
+		alert("Your mmr is: " + parseInt(mmr) );
+		calibrated = true;
 	}
+
 	matches++;
 	kdaCount++;
+	games[matches] = [kda[0],kda[1],kda[2],hero,result];
+	console.log(games[matches]);
+	db();
 	write();
 }
 
 var calibratingWin;
-var calibratingMatches = 0;;
 var calibratingKDA = [ ];
 
 function rankedGame() {
@@ -149,27 +147,29 @@ function rankedGame() {
 	if (matches < 15) {
 		alert("You need at least fifteen matches to calibrate your MMR.")
 	}
-	else {
-	game(true,true)	
+	if (calibratingMatches < 11) {
+		game(false, true);
 	}
-	
+	if (calibratingMatches > 10) {
+		game(true, true);
+	}
 
-	}
+}
 			
 
 
 
 function redditPost() {
-	reddit = ["reddit post about how OP " + heroes[random(0,109)] + " is ", "reddit post about how overrated " + players[random(0,160)] + " is ", "shoutout thread to " + players[random(0,160)]];
+	reddit = ["reddit post about how OP " + heroes[random(0,109)] + " is", "reddit post about how overrated " + players[random(0,160)] + " is", "shoutout thread to " + players[random(0,160)], "reddit post asking for true solo queue", "reddit post asking for Pit Lord", "reddit post asking for the Axe immortal"];
 	if (random(0,1) === 0) {
 		var gain = random(10,5000)
 		karma += gain;
-		alert("You made a " + reddit[random(0,2)] + " and gained " + gain.toString() + " karma.");
+		alert("You made a " + reddit[random(0,5)] + " and gained " + gain.toString() + " karma.");
 	}
 	else {
 		var gain = random(10,5000)
 		karma -= gain;
-		alert("You made a " + reddit[random(0,2)] + " and lost " + gain.toString() + " karma.");
+		alert("You made a " + reddit[random(0,5)] + " and lost " + gain.toString() + " karma.");
 	}
 	write();
 }
